90337c900efab351cb018ebc096c915e5d5744ce
Leon Wetzel <leonwetzel@gmail.com>
2015-09-27 11:48:55 +0200

    Klasse Transfer aangepast.

--- src/Transfer.java
+++ src/Transfer.java
@@ -4,20 +4,37 @@ import java.sql.Connection;
 import java.sql.Statement;
 
 /**
- * Created by Leon on 25-9-2015.
- * Class for transferring data from the Hashmap to the database
+ * Created by Leon Wetzel
+ * Date of creation 25-9-2015, 14:21
+ *
+ * Authors: Leon Wetzel
+ *
+ * Version: 1.0
+ * Package: default
+ * Class: Transfer
+ * Description:
+ * This class sends the data from Measurements objects to the database
+ *
+ * Changelog:
+ *
  */
 public class Transfer {
 
     // Measurement object
-    private Measurement measurement;
+    private Measurements measurements = null;
     private static Connection conn = null;
 
-    // table name
+    // Table name
     private static final String MEASUREMENT = "Measurement";
 
-    public Transfer(Measurement measurement) {
-        this.measurement = measurement;
+    public Transfer(Measurements measurements) {
+        try {
+            // submission to database
+            this.measurements = measurements;
+            transfer(measurements);
+        } catch (SQLException sqle) {
+            System.out.println("Could not connect to database." + sqle.getMessage());
+        }
     }
 
     /**
@@ -25,67 +42,71 @@ public class Transfer {
      * @param args
      */
     public static void main(String[] args) {
-        // connect to database
-        connect();
 
-        //decompose(hashMap);
     }
-
-    /**
-     * Method to send decomposed data to the database
-
-    public void send(Connection conn) {
-        try {
-            // create a Statement from the connection
-            Statement statement = conn.createStatement();
-
-            statement.executeUpdate("INSERT INTO " + TABLE + " VALUES ()");
-        } catch (SQLException sqle) {
-            System.out.println("SQL error!");
-        }
-    }
-     */
-
-    /**
-     * Method to decompose the data from the hashmap
-
-    public static void decompose(HashMap<String, String> hashMap) {
-        // itereer over de hashmap
-        Iterator it = hashMap.keySet().iterator();
-        while (it.hasNext()) {
-            HashMap.Entry pair = (HashMap.Entry)it.next();
-            //System.out.println(pair.getKey() + " = " + pair.getValue());
-
-            it.remove();
-        }
-    }
-    */
 
     /**
      * Method to transfer the data from the Measurement object into the database
-     * @param conn
      */
-    public void transfer(Connection conn) {
-        // itereer over de hashmap
+    public static void transfer(Measurements measurements) throws SQLException {
+
+        // reset Statement and Connection objects
+        Statement statement = null;
+        Connection conn = null;
+
         try {
             // create a Statement from the connection
-            Statement statement = conn.createStatement();
+            conn = connect();
+            statement = conn.createStatement();
+
+            // prepare statement for execution
+            String s = "INSERT INTO " + MEASUREMENT + "(STN, Date, Time, Temp, Dewp, STP, SLP, VISIB, WDSP, PRCP, SNDP, FRSHTT, CLDC, WNDDIR) " + "VALUES" + "( " + measurements.getStn() + "," + measurements.getDate() + "," + measurements.getTime() + "," + measurements.getTemp() + "," + measurements.getDewp() + "," + measurements.getStp() + "," + measurements.getSlp() + "," + measurements.getVisib() + "," + measurements.getWdsp() + "," + measurements.getPrcp() + "," + measurements.getSndp() + "," + measurements.getFrshtt() + "," + measurements.getCldc() + "," + measurements.getWnddir() + ")";
 
             // send data to database
-            statement.executeUpdate("INSERT INTO " + MEASUREMENT + " VALUES (" + measurement.getSTN(), measurement.getDate(), measurement.getTime(), measurement.getTemp(), measurement.getDewp(), measurement.getSTP(), measurement.getSLP(), measurement.getVISIB(), measurement.WDSP(), measurement.getPRCP(), measurement.getSNDP(), measurement.getFRSHTT(), measurement.getCLDC(), measurement.getWNDDIR()) + ")";
+            statement.executeUpdate(s);
+
         } catch (SQLException sqle) {
-            System.out.println("SQL error!");
+            // connection could not be established
+            System.out.println("Could not connect to database." + sqle.getMessage());
+        } finally {
+            // apply close() method for Statement and Connection objects
+            closeObjects(statement, conn);
         }
+
+
     }
 
     /**
      * Method to connect to database
      */
-    public static void connect() {
+    public static Connection connect() {
         try {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "user","pass");
+            return conn;
         } catch (SQLException sqle) {
             System.out.println("Could not connect to database.");
+            return conn;
+        }
+    }
+
+    /**
+     * Method to close both a Statement and Connection object.
+     * @param statement
+     * @param conn
+     */
+    public static void closeObjects(Statement statement, Connection conn) throws SQLException {
+        try {
+            // close statement if statement object has been used
+            if (statement != null) {
+                statement.close();
+            }
+
+            // close connection if Connection object has been used
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException sqle) {
+            System.out.println("Could not connect to database." + sqle.getMessage());
         }
     }
 }
